2146726f57abf2d764879f240a704781e3ab68bc
Edvan Trindade <trindade3e@gmail.com>
2025-02-05 16:52:19 -0300

    Collision with the floor

---
+++ scripts/bird.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+@onready var _animated_sprite = $AnimatedSprite2D
+const gravity = Vector2(0, 20)
+var impulse = Vector2(0, 600)
+var initial_position = Vector2(200, 20)
+
+func _ready():
+	position = initial_position   
+	
+
+func _process(delta):
+	# start animatition of the bird
+	_animated_sprite.play()
+	
+	# jumping
+	if Input.is_action_just_pressed("jump"):
+		velocity -= impulse
+	
+	# Moving and detecting collisions
+	velocity += gravity
+	var collision = move_and_collide(velocity * delta)
+	
+	# TODO: handle game over in the future sending a signal to the main scene
+	if collision:
+		_animated_sprite.stop()
+		set_process(false)
+		
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_ENTER:
+			if is_processing():
+				_animated_sprite.stop()
+			else:
+				_animated_sprite.play()
+			set_process(not is_processing())
