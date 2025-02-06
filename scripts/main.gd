5dbef0ed74c090becd0cf19cb3989f49060263a9
Edvan Trindade <trindade3e@gmail.com>
2025-02-06 18:11:22 -0300

    Instantiating pipes by code

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,18 +1,23 @@
 extends Node
 
 @onready var bird_collided = get_node("Bird").bird_collided.connect(on_bird_collided)
+@onready var pipe = load("res://scenes/pipe.tscn")
+
 var is_game_over = false
 
 # Signals
 signal game_over
 signal play_pause(is_game_over: bool)
 
-
 			
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	pass # Replace with function body.
+	var pipe_instance = pipe.instantiate()
+	pipe_instance.game_over_signal = game_over
+	pipe_instance.play_pause_signal = play_pause
+	pipe_instance.z_index = -1
+	add_child(pipe_instance)
 	
 func on_bird_collided():
 	play_pause.emit(true)
--- scripts/pipe.gd
+++ scripts/pipe.gd
@@ -7,6 +7,8 @@ var x_pos_up = null
 var y_pos_up = null
 var x_global_pos = null
 var y_global_pos = null
+var game_over_signal = null # getting value from main
+var play_pause_signal = null # getting value from main
 
 func _init(x_up = 0, y_up = -400, x_global = 500, y_global = 400):
 	x_pos_up = x_up
@@ -16,16 +18,16 @@ func _init(x_up = 0, y_up = -400, x_global = 500, y_global = 400):
 	
 func _ready():
 	pipe_up.position = Vector2(x_pos_up, y_pos_up)
+	game_over_signal.connect(_on_main_game_over)
+	play_pause_signal.connect(_on_main_play_pause)
 	position = Vector2(x_global_pos, y_global_pos)
 
 func _process(delta):
 	position += Vector2(-SPEED * delta, 0)
 
 func _on_main_game_over() -> void:
-	print("Game over")
 	set_process(false)
 
 func _on_main_play_pause(is_game_over) -> void:
 	if not is_game_over:
-		print("Not game over")
 		set_process(not is_processing())
